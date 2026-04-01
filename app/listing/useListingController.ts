1a5186a54a8786aede08e3f4680eabe860bb96a1
Nicolás Olmos <nicolasolmoscorreo@gmail.com>
2026-03-31 21:33:39 -0300

    fix error

--- app/listing/useListingController.ts
+++ app/listing/useListingController.ts
@@ -5,16 +5,22 @@ import { createProduct } from "../services/productService";
 import { isCustomErrorPage } from "next/dist/build/utils";
 
 
+
 export function useListingController() {
   const [error, setError] = useState(false);
   const [products, setProducts] = useState<Array<any>>([]);
-  const [newProductData, setNewProductData] = useState(false);
+  const [newProductData, setNewProductData] = useState<any>();
   const [isCreatingProduct, setIsCreatingProduct] = useState(false);
   const [createNewProduct, setCreateNewProduct] = useState(false);
   useEffect(() => {
     if (newProductData) {
       const token = globalThis.localStorage.getItem('token')!;
-      const dataAsString = JSON.stringify({ ...newProductData, categoryId: 1 });
+      const data = {
+        categoryId: 1,
+        title: newProductData.title,
+        description: newProductData.description,
+      };
+      const dataAsString = JSON.stringify(data);
       setIsCreatingProduct(true);
       console.log('llego aca')
       createProduct(dataAsString, token)
